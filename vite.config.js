c4f85fdc8b43a1951398de83b31a4e20b7a0d4d0
Techosols <techosols.co@gmail.com>
2025-12-10 16:22:50 +0500

    fix: comment out port configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port:4280,
+    // port:4280,
     proxy: {
       '/api': {
         target: 'http://localhost:7071/',
