2c559df99664e6a2f9f301547803897fcaea0078
Techosols <techosols.co@gmail.com>
2025-12-10 12:36:05 +0500

    feat: integrate useOption hook to fetch suggestion categories in ProfileSuggestion component

--- src/components/models/ProfileSuggestion.jsx
+++ src/components/models/ProfileSuggestion.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import useOption from '../../hooks/useOption'
 
 function ProfileSuggestion() {
+  const { suggestionCategories } = useOption();
+
+  console.log('🔴 Suggestion Categories inside model :', suggestionCategories);
+
   return (
     <div>
       <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-100">Profile Suggestion</h3>
